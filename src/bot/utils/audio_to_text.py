fcad4819e38e1a1fed389a95a20c305a78add969
CoffeeCat <mihail.kuzikov@yandex.ru>
2025-02-17 22:31:06 +0300

    add 2 (#1)

--- src/bot/utils/audio_to_text.py
+++ src/bot/utils/audio_to_text.py
@@ -5,5 +5,5 @@ async def audio_to_text(bot: Bot, file_id) -> str:
     # конвертировать аудио в текст
     # bot передается для скачивания файла по file_id
 
-    text = "конвертированный текст"
+    text = "конвертированный текст 2"
     return text
